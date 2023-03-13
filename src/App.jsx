8b7df8f7e19defd273d0375ee6c21c98a2f0e82e
Rajat Dash <rajat@192.168.1.27>
2023-03-13 19:59:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './Home'
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello geeksters</h1>
+      <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import imggggggg from './assets/randomimg.avif';
+import './Home.css'
+
+const Home = () => {
+  return (
+    <>
+        <h1 className='home' id='home2'>Hello from home</h1>
+
+        <img src={imggggggg} alt="img" />
+        
+    </>
+  )
+}
+
+export default Home
